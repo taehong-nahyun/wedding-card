3b1e4302f62dcc930b8d2384f1dd528bdf60e589
jinsu-dain <58765619+jinsu-dain@users.noreply.github.com>
2020-08-29 09:39:01 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -384,7 +384,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2020/9/4', function(event) {
+        $('#clock').countdown('February 29 2020 16:00:00 GMT+0900', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
